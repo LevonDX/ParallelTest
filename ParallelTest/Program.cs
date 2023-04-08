13a9295d5612a1ea8a3b70bcd8696424ddbe7909
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-04-08 12:30:04 +0400

    Add project files.

---
+++ ParallelTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace ParallelTest
+{
+    internal class Program
+    {
+        static void F()
+        {
+            Thread.Sleep(1000);
+        }
+
+        static void Main()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            //Parallel.Invoke(F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F);
+
+            int elementCount = (int)1000;
+            double[] elementArray = new double[elementCount];
+
+            Parallel.For(0, elementCount, i => elementArray[i] = i);
+
+            stopwatch.Stop();
+
+            Parallel.ForEach(elementArray, i => Console.WriteLine(i));
+
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
